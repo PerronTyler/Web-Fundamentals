38439e12d31122627ae5d7186efe27c937c9c7ab
Tyler Perron <tperron31313@gmail.com>
2022-09-19 21:12:14 -0400

    First Commit

---
+++ Video Preview (JS)/script.js
@@ -0,0 +1,6 @@
+console.log("page loaded...");
+function embedVideo() {
+    var x = document.getElementById("example");
+    document.getElementById("demo").innerHTML = x;
+  }
+  document.getElementById("example").muted = true;
